0e9f4999155fa23fbef17318b88786eb903e25a6
Cordell Hurst <cordellhurst@gmail.com>
2017-12-28 10:24:19 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 	This program is tasked to read the input file, parse its lines, construct a Tree object for each line
 	and make the calls to the Tree_Collection class to insert tree objects into the collection. As each line of data
 	is read, its 41 fields are separated and the proper subset of nine of them are used to construct the
-	Tree object. Tree_Collection, uses the "spc_common/tree_id" pair as a unique key for inserting
+	Tree object. Tree_Collection uses the "spc_common/tree_id" pair as a unique key for inserting
 	the Tree objects into the encapsulated AVL tree. The TreeCollection also keeps track of the names of
 	all species that are stored and in which boroughs the trees are located using <set>. AVL_Tree holds all 
 	complete trees from the census. Any blank feilds that have been declared blank or incorrectly may 
